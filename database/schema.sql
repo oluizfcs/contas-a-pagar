08fb03c4c3c04d21fe324cb5bdcb110a9b273c63
oluizfcs <luizfelipexilipe@gmail.com>
2025-08-26 11:07:25 -0300

    feat: enabled flag

--- database/schema.sql
+++ database/schema.sql
@@ -10,22 +10,25 @@ CREATE TABLE usuario(
     nome VARCHAR(45) NOT NULL,
     senha VARCHAR(255) NOT NULL,
     data_criacao DATETIME NOT NULL DEFAULT NOW(),
-    data_edicao DATETIME ON UPDATE NOW()
+    data_edicao DATETIME ON UPDATE NOW(),
+    `enabled` TINYINT NOT NULL DEFAULT 1
 ) ENGINE=INNODB;
 
-CREATE TABLE conta(
+CREATE TABLE banco(
     id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(45) NOT NULL,
     saldo_em_centavos INT NOT NULL,
     data_criacao DATETIME NOT NULL DEFAULT NOW(),
-    data_edicao DATETIME ON UPDATE NOW()
+    data_edicao DATETIME ON UPDATE NOW(),
+    `enabled` TINYINT NOT NULL DEFAULT 1
 ) ENGINE=INNODB;
 
 CREATE TABLE centro_de_custo(
     id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(45) NOT NULL,
     data_criacao DATETIME NOT NULL DEFAULT NOW(),
-    data_edicao DATETIME ON UPDATE NOW()
+    data_edicao DATETIME ON UPDATE NOW(),
+    `enabled` TINYINT NOT NULL DEFAULT 1
 ) ENGINE=INNODB;
 
 CREATE TABLE fornecedor(
@@ -33,15 +36,11 @@ CREATE TABLE fornecedor(
     nome VARCHAR(55) NOT NULL,
     telefone VARCHAR(45),
     data_criacao DATETIME NOT NULL DEFAULT NOW(),
-    data_edicao DATETIME ON UPDATE NOW()
+    data_edicao DATETIME ON UPDATE NOW(),
+    `enabled` TINYINT NOT NULL DEFAULT 1
 ) ENGINE=INNODB;
 
-CREATE TABLE status(
-    id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(45) NOT NULL
-) ENGINE=INNODB;
-
-CREATE TABLE despesa(
+CREATE TABLE conta(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     descricao VARCHAR(500),
     valor_em_centavos INT UNSIGNED NOT NULL,
@@ -49,15 +48,15 @@ CREATE TABLE despesa(
     data_criacao DATETIME NOT NULL DEFAULT NOW(),
     data_edicao DATETIME ON UPDATE NOW(),
     usuario_id SMALLINT UNSIGNED NOT NULL,
-    conta_id SMALLINT UNSIGNED NOT NULL,
+    banco_id SMALLINT UNSIGNED NOT NULL,
     centro_de_custo_id SMALLINT UNSIGNED NOT NULL,
     fornecedor_id MEDIUMINT UNSIGNED NOT NULL,
-    status_id TINYINT UNSIGNED NOT NULL,
+    paid TINYINT NOT NULL DEFAULT 0,
+    `enabled` TINYINT NOT NULL DEFAULT 1,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id),
-    FOREIGN KEY (conta_id) REFERENCES conta(id),
+    FOREIGN KEY (banco_id) REFERENCES banco(id),
     FOREIGN KEY (centro_de_custo_id) REFERENCES centro_de_custo(id),
-    FOREIGN KEY (fornecedor_id) REFERENCES fornecedor(id),
-    FOREIGN KEY (status_id) REFERENCES status(id)
+    FOREIGN KEY (fornecedor_id) REFERENCES fornecedor(id)
 ) ENGINE=INNODB;
 
 CREATE TABLE parcela(
@@ -66,19 +65,18 @@ CREATE TABLE parcela(
     valor_em_centavos INT UNSIGNED NOT NULL,
     data_vencimento DATE NOT NULL,
     data_pagamento DATE,
-    data_criacao DATETIME NOT NULL DEFAULT NOW(),
-    data_edicao DATETIME ON UPDATE NOW(),
-    despesa_id INT UNSIGNED NOT NULL,
-    status_id TINYINT UNSIGNED NOT NULL,
-    FOREIGN KEY (despesa_id) REFERENCES despesa(id),
-    FOREIGN KEY (status_id) REFERENCES status(id)
+    conta_id INT UNSIGNED NOT NULL,
+    paid TINYINT NOT NULL DEFAULT 0,
+    FOREIGN KEY (conta_id) REFERENCES conta(id)
 ) ENGINE=INNODB;
+
+/*///////////////////////////////// LOGS /////////////////////////////////////// */
 
 CREATE TABLE log_usuario(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     campo VARCHAR(45) NOT NULL,
-    valor_antigo VARCHAR(45) NOT NULL,
-    valor_novo VARCHAR(45) NOT NULL,
+    valor_antigo VARCHAR(45),
+    valor_novo VARCHAR(45),
     data_log DATETIME DEFAULT NOW(),
     usuario_id SMALLINT UNSIGNED NOT NULL,
     usuario_editado_id SMALLINT UNSIGNED NOT NULL,
@@ -86,23 +84,23 @@ CREATE TABLE log_usuario(
     FOREIGN KEY (usuario_editado_id) REFERENCES usuario(id)
 ) ENGINE=INNODB;
 
-CREATE TABLE log_conta(
+CREATE TABLE log_banco(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     campo VARCHAR(45) NOT NULL,
-    valor_antigo VARCHAR(45) NOT NULL,
-    valor_novo VARCHAR(45) NOT NULL,
+    valor_antigo VARCHAR(45),
+    valor_novo VARCHAR(45),
     data_log DATETIME DEFAULT NOW(),
     usuario_id SMALLINT UNSIGNED NOT NULL,
-    conta_id SMALLINT UNSIGNED NOT NULL,
+    banco_id SMALLINT UNSIGNED NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id),
-    FOREIGN KEY (conta_id) REFERENCES conta(id)
+    FOREIGN KEY (banco_id) REFERENCES banco(id)
 ) ENGINE=INNODB;
 
 CREATE TABLE log_fornecedor(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     campo VARCHAR(45) NOT NULL,
-    valor_antigo VARCHAR(55) NOT NULL,
-    valor_novo VARCHAR(55) NOT NULL,
+    valor_antigo VARCHAR(55),
+    valor_novo VARCHAR(55),
     data_log DATETIME DEFAULT NOW(),
     usuario_id SMALLINT UNSIGNED NOT NULL,
     fornecedor_id MEDIUMINT UNSIGNED NOT NULL,
@@ -113,8 +111,8 @@ CREATE TABLE log_fornecedor(
 CREATE TABLE log_centro_de_custo(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     campo VARCHAR(45) NOT NULL,
-    valor_antigo VARCHAR(45) NOT NULL,
-    valor_novo VARCHAR(45) NOT NULL,
+    valor_antigo VARCHAR(45),
+    valor_novo VARCHAR(45),
     data_log DATETIME DEFAULT NOW(),
     usuario_id SMALLINT UNSIGNED NOT NULL,
     centro_de_custo_id SMALLINT UNSIGNED NOT NULL,
@@ -122,26 +120,14 @@ CREATE TABLE log_centro_de_custo(
     FOREIGN KEY (centro_de_custo_id) REFERENCES centro_de_custo(id)
 ) ENGINE=INNODB;
 
-CREATE TABLE log_despesa(
+CREATE TABLE log_conta(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     campo VARCHAR(500) NOT NULL,
-    valor_antigo VARCHAR(500) NOT NULL,
-    valor_novo VARCHAR(500) NOT NULL,
+    valor_antigo VARCHAR(500),
+    valor_novo VARCHAR(500),
     data_log DATETIME DEFAULT NOW(),
     usuario_id SMALLINT UNSIGNED NOT NULL,
-    despesa_id INT UNSIGNED NOT NULL,
+    conta_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id),
-    FOREIGN KEY (despesa_id) REFERENCES despesa(id)
-) ENGINE=INNODB;
-
-CREATE TABLE log_parcela(
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    campo VARCHAR(45) NOT NULL,
-    valor_antigo VARCHAR(45) NOT NULL,
-    valor_novo VARCHAR(45) NOT NULL,
-    data_log DATETIME DEFAULT NOW(),
-    usuario_id SMALLINT UNSIGNED NOT NULL,
-    parcela_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES usuario(id),
-    FOREIGN KEY (parcela_id) REFERENCES parcela(id)
+    FOREIGN KEY (conta_id) REFERENCES conta(id)
 ) ENGINE=INNODB;
